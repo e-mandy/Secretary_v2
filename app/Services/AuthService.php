aa86ef737571abb8775e6ce47d02bf287aba2aef
e-mandy <andymfrd02@gmail.com>
2026-03-10 14:10:14 +0000

    feat(login-secretary): Set up of the service for the login as a secretary

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,14 +3,14 @@
 namespace App\Services;
 
 use App\DTOs\Auth\AdminDTO;
+use App\DTOs\Auth\LoginSecretaryDTO;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class AuthService{
 
     public function loginAsAdmin(AdminDTO $data){
-
-        // Check if the email exist in the database
+        // Check if the email exists in the database
         $user = User::where('email', $data->email)->first();
         
         if(!$user) throw new \Exception("Données invalides");
@@ -18,13 +18,39 @@ class AuthService{
         $isMatch = Hash::check($data->password, $user->password);
         if(!$isMatch) throw new \Exception("Données invalides");
 
-        $token = $user->createToken("User: " . $user->email);
+        $token = $user->createToken(
+            "Token Connexion User: " . $user->email,
+            ["*"]
+        );
 
         return [
             "user" => [
                 "email" => $user->email,
                 "lastname" => $user->lastname,
-                "firstname" => $user->firstname
+                "firstname" => $user->firstname,
+                "role" => $user->role
+            ],
+            "access_token" => $token
+        ];
+    }
+
+    public function loginAsSecretary(LoginSecretaryDTO $data){
+        // Check if the email exists in the database
+        $user = User::where('email', $data->email)->first();
+
+        if(!$user) throw new \Exception("Données invalides");
+
+        $isMatch = Hash::check($data->password, $user->password);
+        if(!$isMatch) throw new \Exception("Données invalides");
+
+        $token = $user->createToken("Token Connexion User: " . $user->email);
+
+        return [
+            "user" => [
+                "email" => $user->email,
+                "lastname" => $user->lastname,
+                "firstname" => $user->firstname,
+                "role" => $user->role
             ],
             "access_token" => $token
         ];
