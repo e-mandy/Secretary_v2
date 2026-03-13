bd424485ff52cbd0e2865aa3b8534ff7f7d94aa9
e-mandy <andymfrd02@gmail.com>
2026-03-13 02:16:30 +0000

    feat(auth-store): Set up of the schema for the authentication store

---
+++ src/features/auth/schemas/auth_store.schema.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+
+const userSchema = z.object({
+    lastname: z.string(),
+    firstname: z.string(),
+    email: z.email(),
+    role: z.enum(["secretariat", "administration"]).nullable()
+})
+
+const authStoreSchema = z.object({
+    user: userSchema.nullable(),
+    token: z.string().nullable()
+});
+
+export type UserType = z.infer<typeof userSchema>;
+
+export type AuthStoreType = z.infer<typeof authStoreSchema>
