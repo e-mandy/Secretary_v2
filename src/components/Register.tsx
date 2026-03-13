739f329dd71c18e3c3f2f3521fea3710d8589d90
e-mandy <andymfrd02@gmail.com>
2026-03-13 11:23:19 +0000

    feat(register): Set up of the logic of sending data through the form

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,5 +1,25 @@
+import { Mail } from "lucide-react";
+import { useRegister } from "../features/auth/api/useRegister";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { registerSchema, type RegisterUser } from "../features/auth/schemas/register.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+    const { mutate, isPending } = useRegister();
+
+    const navigate = useNavigate();
+
+    const { handleSubmit, register, formState: { errors }, reset } = useForm<RegisterUser>({
+        resolver: zodResolver(registerSchema)
+    });
+
+    const onSubmit: SubmitHandler<RegisterUser> = async (registerFormData) => {
+        mutate(registerFormData);
+        reset();
+        navigate('/');
+    }
+
   return (
     <div className="h-screen relative flex">
         <div className="absolute flex top-4 left-4">
@@ -12,12 +32,54 @@ const Register = () => {
             </div>
         </div>
         <div className="w-1/2 h-screen flex">
-            <div className="m-auto w-3/4">
-                <h2 className="text-2xl font-bold">Inscription</h2>
-                <p>Bienvenue sur la plateforme de gestion du sécrétariat de Esgis</p>
-            </div>
-            <div>
+            <div className="m-auto w-[55%]">
+                <div className="mb-4">
+                    <h2 className="text-4xl font-extrabold">Inscription</h2>
+                    <p>Bienvenue sur la plateforme de gestion du sécrétariat de Esgis</p>
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="my-3">
+                        <label htmlFor="lastname" className="font-semibold">Nom de famille</label>
+                        <div className="input-style">
+                            <Mail />
+                            <input {...register('lastname')} className="outline-none flex-1 py-3 pl-2" type="text" name="lastname" id="lastname" />
+                        </div>
+                    </div>
 
+                    <div className="my-3">
+                        <label htmlFor="firstname" className="font-semibold">Nom de famille</label>
+                        <div className="input-style">
+                            <Mail />
+                            <input {...register('firstname')} className="outline-none flex-1 py-3 pl-2" type="text" name="firstname" id="firstname" />
+                        </div>
+                    </div>
+
+                    <div className="my-3">
+                        <label htmlFor="email" className="font-semibold">Nom de famille</label>
+                        <div className="input-style">
+                            <Mail />
+                            <input {...register('email')} className="outline-none flex-1 py-3 pl-2" type="text" name="email" id="email" />
+                        </div>
+                    </div>
+
+                    <div className="my-3">
+                        <label htmlFor="password" className="font-semibold">Nom de famille</label>
+                        <div className="input-style">
+                            <Mail />
+                            <input {...register('password')} className="outline-none flex-1 py-3 pl-2" type="text" name="password" id="password" />
+                        </div>
+                    </div>
+
+                    <div className="my-3">
+                        <label htmlFor="password_confirmation" className="font-semibold">Nom de famille</label>
+                        <div className="input-style">
+                            <Mail />
+                            <input {...register('password_confirmation')} className="outline-none flex-1 py-3 pl-2" type="text" name="password_confirmation" id="password_confirmation" />
+                        </div>
+                    </div>
+                    <button className="bg-[#111624] w-full text-white py-3 rounded-lg my-3">S'inscrire en tant que sécrétaire</button>
+                    <p>Vous avez déjà un compte sécrétaire ? <span>Connectez-vous !!</span></p>
+                </form>
             </div>
         </div>
     </div>
