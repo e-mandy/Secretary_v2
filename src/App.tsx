b52c996b8dc6230b1da20a9a3b4be11720befea9
e-mandy <andymfrd02@gmail.com>
2026-03-23 03:55:56 +0000

    feat(layout): Set up of the main structure of the app layout

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,17 @@ function App() {
         <Route path="/secretary/email-verify/:id/:hash" element={<EmailVerify />} />
         <Route element={<PersistLogin />}>
 
+        {/** We have to set up the default layout for the app. */}
+          <div className="w-screen flex relative">
+            <div className="w-72 fixed left-0 bg-red-300">
+
+            </div>
+            <div className="md:ml-72 relative w-full min-h-screen bg-yellow-300">
+              <div className="fixed w-full bg-blue-300">
+
+              </div>
+            </div>
+          </div>
         </Route>
       </Routes>
     </BrowserRouter>
