01696a7613d0ce56df54387de3a9722259e02f7f
e-mandy <andymfrd02@gmail.com>
2026-04-19 18:44:54 +0000

    fix(matters): Fixed the eloquent request

--- app/Services/MatterService.php
+++ app/Services/MatterService.php
@@ -2,12 +2,12 @@
 
 namespace App\Services;
 
-use App\Http\Resources\Matter\MatterResource;
+use App\Http\Resources\MatterResource;
 use App\Models\Matter;
 
 class MatterService{
     public function index(){
-        $matters = Matter::select("id", "name");
+        $matters = Matter::select("id", "name")->get();
 
         return MatterResource::collection($matters);
     }
